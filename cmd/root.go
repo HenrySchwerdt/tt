9461254cf3c78792b9411536762f101f635b1a7d
Henry Schwerdtner <henry.schwerdtner@web.de>
2025-12-07 14:11:13 +0100

    test

--- cmd/root.go
+++ cmd/root.go
@@ -1,10 +1,32 @@
-package main
+package cmd
 
 import (
+	"os"
+	"path/filepath"
+	"runtime"
+
 	"github.com/spf13/cobra"
 )
 
 var RootCmd = &cobra.Command{
 	Use:   "tt",
 	Short: "Terminal Time Tracker",
+}
+
+func DefaultDBPath() string {
+	var configDir string
+
+	if runtime.GOOS == "windows" {
+		configDir = os.Getenv("APPDATA") // typically C:\Users\<User>\AppData\Roaming
+	} else {
+		configDir = os.Getenv("XDG_CONFIG_HOME")
+		if configDir == "" {
+			configDir = filepath.Join(os.Getenv("HOME"), ".config")
+		}
+	}
+
+	dbDir := filepath.Join(configDir, "tt")
+	_ = os.MkdirAll(dbDir, os.ModePerm) // ensure folder exists
+
+	return filepath.Join(dbDir, "tt.db")
 }
